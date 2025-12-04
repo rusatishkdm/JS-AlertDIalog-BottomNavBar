490300d6ca268f6c30edd14c2dcddb9fc8f48a3c
Satish R <satish.ujjammanavar@myhealthcare.life>
2025-12-04 13:14:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/jc_ui_components_bottom_nav_bar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jc_ui_components_bottom_nav_bar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jc_ui_components_bottom_nav_bar.ui.theme.JCAlertDIalogBottomNavBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JCAlertDIalogBottomNavBarTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JCAlertDIalogBottomNavBarTheme {
+        Greeting("Android")
+    }
+}
